480de2f9aa271610d6bd48b5370b98f4761b3f73
nick <gnn@mm.st>
2020-04-18 16:42:58 +0300

    add service

--- src/main/java/my/test/taskSberClient/service/impl/ServerApiServiceImpl.java
+++ src/main/java/my/test/taskSberClient/service/impl/ServerApiServiceImpl.java
@@ -1,5 +1,6 @@
-package my.test.taskSberClient.service;
+package my.test.taskSberClient.service.impl;
 
+import my.test.taskSberClient.service.ServerApiService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.PropertySource;
@@ -8,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 @PropertySource("classpath:application.properties")
-public class ServerApiServiceImpl implements ServerApiService{
+public class ServerApiServiceImpl implements ServerApiService {
     private RestTemplate restTemplate;
 
     @Value("${serverUrl}")
@@ -21,6 +22,6 @@ public class ServerApiServiceImpl implements ServerApiService{
     @Override
     public String sendMessageXML(String message) {
         String URL = serverPath + "/api/getMessages/messageXML";
-        return restTemplate.postForObject(URL, null, String.class);
+        return restTemplate.postForObject(URL, message, String.class);
     }
 }
