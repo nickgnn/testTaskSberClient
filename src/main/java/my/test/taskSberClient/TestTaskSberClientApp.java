6a95ac75416f3f68263ea9da0c00fc336139ba3a
nick <gnn@mm.st>
2020-04-18 16:43:37 +0300

    add launcher

--- src/main/java/my/test/taskSberClient/TestTaskSberClientApp.java
+++ src/main/java/my/test/taskSberClient/TestTaskSberClientApp.java
@@ -1,4 +1,11 @@
-package my.test.taskSber;
+package my.test.taskSberClient;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class TestTaskSberClientApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskSberClientApp.class, args);
+    }
 }
