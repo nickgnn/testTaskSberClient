78d6fa47bf20a66dcc8cb52b185e3bf8aab2cc4a
nick <gnn@mm.st>
2020-04-19 19:33:21 +0300

    change port

--- src/main/java/my/test/taskSberClient/service/impl/StarterGrpcServerServiceImpl.java
+++ src/main/java/my/test/taskSberClient/service/impl/StarterGrpcServerServiceImpl.java
@@ -3,20 +3,24 @@ package my.test.taskSberClient.service.impl;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import my.test.taskSberClient.service.StarterGrpcServerService;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
 @Service
 public class StarterGrpcServerServiceImpl implements StarterGrpcServerService {
-    @Value("${server.port}")
-    private String serverPort;
+    private GreetingServiceImpl greetingService;
+
+    @Autowired
+    public StarterGrpcServerServiceImpl(GreetingServiceImpl greetingService) {
+        this.greetingService = greetingService;
+    }
 
     @Override
     public void startServer() throws IOException, InterruptedException{
-        Server server = ServerBuilder.forPort(Integer.valueOf(serverPort))
-                .addService(new GreetingServiceImpl()).build();
+        Server server = ServerBuilder.forPort(8080)
+                .addService(greetingService).build();
 
         System.out.println("Starting server...");
         server.start();
