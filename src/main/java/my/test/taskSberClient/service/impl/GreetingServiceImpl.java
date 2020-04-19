800ed738b0b4189b302fbe75d6c21d0114bde214
nick <gnn@mm.st>
2020-04-19 15:44:56 +0300

    override getCurName()

--- src/main/java/my/test/taskSberClient/service/impl/GreetingServiceImpl.java
+++ src/main/java/my/test/taskSberClient/service/impl/GreetingServiceImpl.java
@@ -1,4 +1,22 @@
 package my.test.taskSberClient.service.impl;
 
-public class GreetingServiceImpl {
+import com.example.grpc.GreetingServiceGrpc;
+import com.example.grpc.MyRequest;
+import com.example.grpc.MyResponse;
+import io.grpc.stub.StreamObserver;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
+    @Override
+    public void getCurName(MyRequest request, StreamObserver<MyResponse> responseObserver) {
+        System.out.println(request);
+
+        String iso4217 = "Hello there, " + request.getCurName();
+
+        MyResponse response = MyResponse.newBuilder().setIso4217(iso4217).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
